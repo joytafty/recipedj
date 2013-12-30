1d82ec04c687abca1a33560871c414186062290c
joytafty <joytafty@gmail.com>
2013-12-29 23:39:09 -0500

    update Procfile

--- web.js
+++ web.js
@@ -1,14 +1,23 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Welcome to recipeDJ App!');
-});
-
+var http = require('http');
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.CreateServer(function(req, res) {
+	res.writeHead(200);
+	res.write("Welcome to recipeDJ WebApp");
+	res.end(); 
+}).listen(port);
+console.log('Listening on ' + port);
+
+// var express = require("express");
+// var logfmt = require("logfmt");
+// var app = express();
+
+// app.use(logfmt.requestLogger());
+
+// app.get('/', function(req, res) {
+//   res.send('Welcome to recipeDJ App!');
+// });
+
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
