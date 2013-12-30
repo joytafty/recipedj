5cb894e8d79493a188c99243ed0cd4651b0e991e
joytafty <joytafty@gmail.com>
2013-12-30 10:32:23 -0500

    update routes

--- web.js
+++ web.js
@@ -14,53 +14,53 @@ var express = require('express'),
  
 var fs = require('fs');
 
-function handleStaticPages(pathName, res) {
-	var ext = path.extname(pathName);
-	switch(ext) {
-		case '.css':
-			res.writeHead(200, {'Content-Type' : 'text/css'});
-			fs.readFile('.' + pathName, 'utf8', function(err, fd){
-				res.end(fd);
-			});
-			console.log('Routed for CSS ' + pathName + ' Successfully\n');
-		break;
-		case '.js';		
-			res.writeHead(200, {'Content-Type':, 'text/javascript'});
-			fs.readFile('./' + pathName, 'utf8', function(err, fd){
-				res.end(fd);
-			});
-			console.log('Routed for Javascript ' + pathName + ' Successfully\n');
-		break;
-	}
+// function handleStaticPages(pathName, res) {
+// 	var ext = path.extname(pathName);
+// 	switch(ext) {
+// 		case '.css':
+// 			res.writeHead(200, {'Content-Type' : 'text/css'});
+// 			fs.readFile('.' + pathName, 'utf8', function(err, fd){
+// 				res.end(fd);
+// 			});
+// 			console.log('Routed for CSS ' + pathName + ' Successfully\n');
+// 		break;
+// 		case '.js';		
+// 			res.writeHead(200, {'Content-Type':, 'text/javascript'});
+// 			fs.readFile('./' + pathName, 'utf8', function(err, fd){
+// 				res.end(fd);
+// 			});
+// 			console.log('Routed for Javascript ' + pathName + ' Successfully\n');
+// 		break;
+// 	}
 
-}
+// }
 
-function start(route, handle) {
-	function onRequest(req, res) {
-		var postData = "";
-		var pathName = url.par(req.url).pathname;
-		console.log('Request for ' + pathName + ' received.');
+// function start(route, handle) {
+// 	function onRequest(req, res) {
+// 		var postData = "";
+// 		var pathName = url.par(req.url).pathname;
+// 		console.log('Request for ' + pathName + ' received.');
 
-		req.addListener('data', function(data) {
-			postData += data;
-			console.log('Received POST data chunk ' + postData + '.');
-		});
+// 		req.addListener('data', function(data) {
+// 			postData += data;
+// 			console.log('Received POST data chunk ' + postData + '.');
+// 		});
 
-		req.addListener('end', function(data){
-			var pathext = path.extname(pathName);
-			if (pathext === '.js' || pathext === '.css') {
-				handleStaticPages(pathName, res);
-			} else {
-				console.log('---> Routing only view pages');
-				route(handle, pathName, res, postData);
-			}
-		});
-	}
-	http.createServer(onRequest).listen(4000);
-	console.log('Listening on port 4000');
-}
+// 		req.addListener('end', function(data){
+// 			var pathext = path.extname(pathName);
+// 			if (pathext === '.js' || pathext === '.css') {
+// 				handleStaticPages(pathName, res);
+// 			} else {
+// 				console.log('---> Routing only view pages');
+// 				route(handle, pathName, res, postData);
+// 			}
+// 		});
+// 	}
+// 	http.createServer(onRequest).listen(4000);
+// 	console.log('Listening on port 4000');
+// }
 
-exports.start = start;
+// exports.start = start;
 // Create app
 var app = express.createServer();
 app.use(logfmt.requestLogger());
